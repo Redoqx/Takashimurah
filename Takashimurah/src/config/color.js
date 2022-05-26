3605789b50c3470b93cfd85351b52e484a8f34bc
Redoqx <ridho.pmswa@gmail.com>
2022-05-26 15:42:47 +0700

    Header

--- Takashimurah/src/config/color.js
+++ Takashimurah/src/config/color.js
@@ -1,6 +1,6 @@
-const Color = {
+export default {
     iconColor : "#C7E5BF",
     headerFooterColor: "#48696E",
     backgroundColor: "white",
     panelColor: "#F3FFED",
-}
+};
---
+++ Takashimurah/src/screens/Home.js
@@ -0,0 +1,14 @@
+import{
+    View
+} from 'react-native';
+import Header from "./Header";
+
+const Home = () => {
+    return(
+        <View>
+            <Header/>
+        </View>
+    )
+}
+
+export default Home;
